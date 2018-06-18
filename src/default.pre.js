1722f0decbcdd14c5117da3a3bcc1ec29b914601
Alexandre Capt <acapt@adobe.com>
2018-06-18 15:38:27 +0200

    Refactor Navigation Include to use ESI

--- src/default.pre.js
+++ src/default.pre.js
@@ -1,6 +1,5 @@
 const moment = require('moment');
 const request = require('request-promise');
-const md2json = require('md2json');
 
 /**
  * Appends the context path to the resource based on the strain
