530ded2ffb56fb81a03f388abd495d9e50096273
Alexandre Capt <acapt@adobe.com>
2018-08-13 16:31:20 +0200

    Adjusting to new pre style

--- src/json.pre.js
+++ src/json.pre.js
@@ -17,28 +17,19 @@ function removePosition(node) {
 // module.exports.pre is a function (taking next as an argument)
 // that returns a function (with payload, secrets, logger as arguments)
 // that calls next (after modifying the payload a bit)
-function pre(next) {
-  return async function process(payload, secrets, logger) {
-    try {
-      const p = payload;
+function pre(payload) {
+  const p = payload;
 
-      delete p.resource.body;
-      delete p.resource.html;
+  delete p.resource.body;
+  delete p.resource.html;
 
-      removePosition(p.resource.mdast);
-      removePosition(p.resource.htast);
+  removePosition(p.resource.mdast);
+  removePosition(p.resource.htast);
 
-      let jsonStr = JSON.stringify(p);
-      p.json = jsonStr;
+  let jsonStr = JSON.stringify(p);
+  p.json = jsonStr;
 
-      return next(p, secrets, logger);
-    } catch (e) {
-      logger.error(`Error while executing html.pre.js: ${e.stack || e}`);
-      return {
-        error: e,
-      };
-    }
-  };
+  return p;
 }
 
 module.exports.pre = pre;
